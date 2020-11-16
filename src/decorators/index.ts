dd31e3d8c5c65d503b0deb3c952f7378b55db4e5
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-11-16 03:44:28 +0000

    decorator wrap up

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,13 +1,24 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
   @logError('The boat was sunk')
-  pilot(): void {
-    throw new Error();
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    // throw new Error();
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('...');
+    }
   }
 }
 
@@ -25,4 +36,18 @@ function logError(errorMessage: string) {
   };
 }
 
-new Boat().pilot();
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  // console.log(target.color); // won't work becuase target is prototype with methods definition.
+
+  console.log(key);
+}
+
+// new Boat().pilot();
